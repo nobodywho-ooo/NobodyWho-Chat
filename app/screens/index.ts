d26617af3b24f10b33b28e58ee096bd9d53791eb
Pierre Bresson <262128333+pielouNW@users.noreply.github.com>
2026-06-08 12:08:48 +0200

    rename NoModelDownloaded

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -2,7 +2,7 @@ export * from './ChatScreen/ChatScreen';
 export * from './ErrorScreen/ErrorScreen';
 export * from './LoadingScreen/LoadingScreen';
 export * from './SettingsScreen/SettingsScreen';
-export * from './NoModelDownloaded/NoModelDownloadedScreen';
+export * from './NoModelDownloadedScreen/NoModelDownloadedScreen';
 export * from './ModelsScreen/ModelsScreen';
 export * from './DownloadedModelsScreen/DownloadedModelsScreen';
 export * from './DrawerContentScreen/DrawerContentScreen';
